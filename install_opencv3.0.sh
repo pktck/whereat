f7bd5b876f9ee9aa1e690598cbe406c712cddbdf
Paul Kastner <misc@paulkastner.com>
2015-12-13 00:50:19 -0800

    updates

--- install_opencv3.0.sh
+++ install_opencv3.0.sh
@@ -7,41 +7,44 @@ pause() {
 }
 
 
-#sudo apt-get -y install build-essential cmake git pkg-config \
-    #libjpeg8-dev libtiff4-dev libjasper-dev libpng12-dev \
-    #libgtk2.0-dev \
-    #libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
-    #libatlas-base-dev gfortran \
-    #python2.7-dev
+sudo apt-get -y install build-essential cmake git pkg-config \
+    libjpeg8-dev libtiff4-dev libjasper-dev libpng12-dev \
+    libgtk2.0-dev \
+    libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
+    libatlas-base-dev gfortran \
+    python2.7-dev \
+    python-opencv \
+    python-tk \
+
+
 
 #pause
 
 cd ~
-#wget https://github.com/Itseez/opencv/archive/3.0.0.zip
-#unzip opencv-3.0.0.zip
+wget https://github.com/Itseez/opencv/archive/3.0.0.zip
+unzip opencv-3.0.0.zip
 cd opencv-3.0.0
-#mkdir build
+mkdir build
 cd build
 
 #pause
 
-#cmake -D CMAKE_BUILD_TYPE=RELEASE \
-    #-D CMAKE_INSTALL_PREFIX=/usr/local \
-    #-D INSTALL_C_EXAMPLES=ON \
-    #-D INSTALL_PYTHON_EXAMPLES=ON \
-    #-D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
-    #-D BUILD_EXAMPLES=ON ..
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D INSTALL_C_EXAMPLES=ON \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+    -D BUILD_EXAMPLES=ON ..
 
 #pause
 
 make -j4
 
-pause
+#pause
 
 sudo make install
 
-pause
+#pause
 
 sudo ldconfig
-
 
