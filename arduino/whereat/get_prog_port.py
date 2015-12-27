427859f1c356f19fdda5ff08b1c43bb5b213bc24
Paul Kastner <misc@paulkastner.com>
2015-12-27 15:07:02 -0800

    updated to autodetect arduino programming port

--- arduino/whereat/get_prog_port.py
+++ arduino/whereat/get_prog_port.py
@@ -4,11 +4,14 @@
 import serial.tools.list_ports_posix
 
 def main():
-    ports = [port[0] for port in serial.tools.list_ports_posix.comports()
+    ports = [port[1] for port in serial.tools.list_ports_posix.comports()
             if port[2] == 'USB VID:PID=2341:003d SNR=853323434323511121E0']
 
     return ports[0]
 
 
 if __name__ == '__main__':
-    print main()
+    port = main()
+    assert port
+    print port
+
