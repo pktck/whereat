d73dc145e3a97fba45db93a1def87e5599230ca4
Paul Kastner <misc@paulkastner.com>
2015-12-27 14:51:04 -0800

    added script to detect arduino prog port

---
+++ arduino/whereat/get_prog_port.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+"""Prints the file descriptor of an attached Arduino Due programming port."""
+
+import serial.tools.list_ports_posix
+
+def main():
+    ports = [port[0] for port in serial.tools.list_ports_posix.comports()
+            if port[2] == 'USB VID:PID=2341:003d SNR=853323434323511121E0']
+
+    return ports[0]
+
+
+if __name__ == '__main__':
+    print main()
